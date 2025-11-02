1669eaa7e56a6a0b6856392155ae06353042f6b4
Elliot Robinson <elliot.robinson92@hotmail.com>
2025-11-02 19:31:00 +0000

    use new resource checker functions

--- _features/text-channels/textChannels.router.js
+++ _features/text-channels/textChannels.router.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const permissionRequired = require("../../_shared/middleware/groupSecurity");
+const checkMessageOwnership = require("../../_shared/middleware/resouce-management/checkMessageOwnership");
 const {
   handleGetAllTextChannels,
   handleCreateTextChannel,
@@ -18,34 +19,22 @@ const textChannelsRouter = Router({ mergeParams: true });
 
 // Text Channel Routes
 textChannelsRouter.get("/", handleGetAllTextChannels);
-textChannelsRouter.post(
-  "/",
-  permissionRequired("admin"),
-  handleCreateTextChannel
-);
+textChannelsRouter.post("/", permissionRequired("admin"), handleCreateTextChannel);
 textChannelsRouter.get("/:textChannelId", handleGetTextChannelById);
-textChannelsRouter.delete(
-  "/:textChannelId",
-  permissionRequired("admin"),
-  handleDeleteTextChannel
-);
-textChannelsRouter.patch(
-  "/:textChannelId",
-  permissionRequired("admin"),
-  handleUpdateTextChannel
-);
+textChannelsRouter.delete("/:textChannelId", permissionRequired("admin"), handleDeleteTextChannel);
+textChannelsRouter.patch("/:textChannelId", permissionRequired("admin"), handleUpdateTextChannel);
 
 // Messages Routes
 textChannelsRouter.get("/:textChannelId/messages", handleGetAllMessages);
 textChannelsRouter.post("/:textChannelId/messages", handleCreateMessage);
 textChannelsRouter.patch(
   "/:textChannelId/messages/:messageId/delete",
-  permissionRequired("admin"),
+  checkMessageOwnership,
   handleDeleteMessage
 );
 textChannelsRouter.patch(
   "/:textChannelId/messages/:messageId/update",
-  permissionRequired("admin"),
+  checkMessageOwnership,
   handleUpdateMessage
 );
 
