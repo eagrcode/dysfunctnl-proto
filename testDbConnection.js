973f95a332408c607cac012f8bc23529c8e20ca7
Elliot Robinson <elliot.robinson92@hotmail.com>
2025-11-30 11:09:50 +0000

    move to root dir due to failing to load env vars

--- testDbConnection.js
+++ testDbConnection.js
@@ -1,10 +1,16 @@
-const pool = require("./utils/db");
+const pool = require("./_shared/utils/db");
 
 async function testConnection() {
   let client;
   try {
     client = await pool.connect();
     console.log("Connection to PostgreSQL DB successful!");
+    console.log("Connection details:", {
+      host: client.host,
+      port: client.port,
+      database: client.database,
+      user: client.user,
+    });
   } catch (err) {
     console.error("Connection error:", err.stack);
   } finally {
