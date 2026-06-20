44737eb225747e6b1b728f881f94bea55573337a
Elliot Robinson <eagr@Mac.lan>
2026-06-20 15:53:44 +0100

    fix(auth): refresh token expiry on save

--- _features/auth/auth.model.js
+++ _features/auth/auth.model.js
@@ -1,5 +1,6 @@
 const pool = require("../../_shared/utils/db");
 const { NotFoundError } = require("../../_shared/utils/errors");
+const customConsoleLog = require("../../_shared/utils/customConsoleLog");
 
 // REGISTRATION
 const registration = async (email, password_hash, first_name, last_name) => {
@@ -38,12 +39,20 @@ const getRefreshToken = async (tokenHash) => {
 
 // ADD REFRESH TOKEN
 const addRefreshToken = async (userId, tokenHash) => {
+  customConsoleLog("Adding refresh token...", userId);
   const query = `
-    INSERT INTO refresh_tokens (user_id, token_hash)
-    VALUES ($1, $2)
+    INSERT INTO refresh_tokens (user_id, token_hash, expires_at)
+    VALUES (
+      $1, 
+      $2, 
+      now() + interval '30 days'
+    )
     ON CONFLICT (user_id)
-    DO UPDATE SET token_hash = EXCLUDED.token_hash
-    RETURNING token_hash
+    DO UPDATE SET 
+      token_hash = EXCLUDED.token_hash,
+      expires_at = EXCLUDED.expires_at,
+      updated_at = now()
+    RETURNING token_hash, expires_at, updated_at
   `;
 
   const result = await pool.query(query, [userId, tokenHash]);
